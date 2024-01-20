410c9d921f77c0f2996dc83dd44354b4805ed591
Dmitry Bilenkin <dbilenkin@yandex.ru>
2024-01-20 13:57:12 +0300

    componrnt Menu was corrected

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,23 +2,15 @@ import { NavLink } from "react-router-dom";
 
 function Menu() {
 
-    const onActiveHandler = ({isActive}) => (isActive ? 'menu__item-active' : '');
+    const onActiveHandler = ({isActive}) => (isActive ? 'menu__item menu__item-active' : 'menu__item');
 
     return(
-        <nav className="menu">
-            <div className="menu__item">
-                <NavLink className={onActiveHandler} to="/">Главная</NavLink>
-            </div>
-            <div className="menu__item">
-                <NavLink className={onActiveHandler} to="/drift">Дрифт-такси</NavLink>
-            </div>
-            <div className="menu__item">
-                <NavLink className={onActiveHandler} to="/timeattack">Time Attack</NavLink>
-            </div>
-            <div className="menu__item">
-                <NavLink className={onActiveHandler} to="/forza">Forza Karting</NavLink>
-            </div>       
-     </nav>
+        <nav className="menu">            
+          <NavLink className={onActiveHandler} to="/">Главная</NavLink>
+          <NavLink className={onActiveHandler} to="/drift">Дрифт-такси</NavLink> 
+          <NavLink className={onActiveHandler} to="/timeattack">Time Attack</NavLink>     
+          <NavLink className={onActiveHandler} to="/forza">Forza Karting</NavLink>  
+        </nav>
     )   
   }
   
